978f2874ea16f572c5183f711256b743819236c2
theld-zinza <the.ld@zinza.com.vn>
2022-08-12 09:13:27 +0700

    fix login feature

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return route('auth.login');
+                return redirect()->route('auth.login');
             }
         }
 
