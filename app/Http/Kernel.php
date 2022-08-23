f434b12c0ad45f64910a55d487ffe9f60c2a4619
theld-zinza <the.ld@zinza.com.vn>
2022-08-23 10:11:42 +0700

    add admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'permission.updateUser' => \App\Http\Middleware\PermissionUpdateUser::class,
+        'admin' => \App\Http\Middleware\CheckAdmin::class,
     ];
 }
